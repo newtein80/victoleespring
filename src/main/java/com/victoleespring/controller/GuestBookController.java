cfbe97feeaeff0ab4383f04da5090b9591dc7e63
parkjongsoo <newtein80@gmail.com>
2019-06-08 15:00:03 +0900

    guestbook controller, view add (none db connect...)

--- src/main/java/com/victoleespring/controller/GuestBookController.java
+++ src/main/java/com/victoleespring/controller/GuestBookController.java
@@ -1,52 +1,49 @@
 package com.victoleespring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.victoleespring.repository.GuestBookDAO;
+import com.victoleespring.vo.GuestBookVO;
 
-@Controller
-@RequestMapping("/main")
+@RequestMapping("/guestbook")
 public class GuestBookController {
 	@Autowired
 	private GuestBookDAO guestBookDAO;
 	
-	@RequestMapping(value="/", method=RequestMethod.GET) // 요청할 때는 http://localhost:8080/guestbook/main/ 와 같이 끝에 /를 붙여야 합니다.
-	public String main(@RequestParam(value="a", required=true, defaultValue="one") String a) {
-		System.out.println(a);
-		return "/WEB-INF/views/index.jsp";
+	@RequestMapping("/list")
+	public String list(Model model) {
+		List<GuestBookVO> list = guestBookDAO.getList();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/guestbook/list.jsp";
 	}
 	
-	@RequestMapping(value="/two", method=RequestMethod.GET)
-	public String main2(@RequestParam(value="a", required=true, defaultValue="two") String a) {
-		System.out.println(a);
-		return "/WEB-INF/views/index.jsp";
+	@RequestMapping("/deleteform")
+	public String deleteform() {
+		return "/WEB-INF/views/guestbook/deleteform.jsp";
 	}
 	
-	/*
-	@RequestMapping(value="/{no}", method=RequestMethod.GET)
-	public String main3(@PathVariable("no") Integer no) {
-		System.out.println(no);
-		return "/WEB-INF/views/index.jsp";
-	}
-	*/
-	@RequestMapping(value="/{no:(?!assets|image|search).*}", method=RequestMethod.GET) // main/ 이후의 문자열 중 assets , image , search 문자열을 제외한 모든 값은 no 변수로 받는다 
-	public String main4(@PathVariable("no") Integer no) {
-		System.out.println(no);
-		return "/WEB-INF/views/index.jsp";
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(@ModelAttribute GuestBookVO vo) {
+		guestBookDAO.insert(vo);
+		return "redirect:/guestbook/list";
 	}
 	
-	@RequestMapping(value="/redirect")
-	public String main5() {
-		return "redirect:/main/";
-	}
-	
-	@RequestMapping(value="/redirect2")
-	public String main6() {
-		return "redirect:/main/two?a=9999";
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public String delete(@RequestParam Integer no, @RequestParam String pwd) {
+		String dbpwd = guestBookDAO.getPwd(no);
+		String parseInputPwd = guestBookDAO.getInputPwd(pwd);
+		
+		if(dbpwd.equals(parseInputPwd)) {
+			guestBookDAO.delete(no);
+		}
+		
+		return "redirect:/guestbook/list";
 	}
 }
---
+++ webapp/WEB-INF/views/guestbook/list.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%
+	pageContext.setAttribute("newLine", "\n");
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>방명록 리스트</title>
+</head>
+<body>
+	<form action="/guestbook/main/add" method="post">
+		<table border="1" style="width: 500px;">
+			<tr>
+				<td>이름</td>
+				<td><input type="text" name="name"></td>
+				<td>비밀번호</td>
+				<td><input type="password" name="pwd"></td>
+			</tr>
+			<tr>
+				<td colspan=4><textarea name="content" cols=60 rows=5></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=4 align=right><input type="submit" VALUE=" 확인 ">
+				</td>
+			</tr>
+		</table>
+	</form>
+	<br>
+	<c:set var="count" value="${fn:length(list)}" />
+	<c:forEach items="${list }" var="vo" varStatus="status">
+		<table style="width:510px; border-width:1px;">
+			<tr>
+				<td>[${count - status.index}]</td>
+				<td>${vo.name }</td>
+				<td>${vo.regDate }</td>
+				<td><a href="/guestbook/main/deleteform?no=${vo.no }">삭제</a></td>
+			</tr>
+			<tr>
+				<!-- 개행(\n)을 JSTL에서 사용할 수 없어서 page context에 다른 변수로 추가해줘야함 -->
+				<td>${fn:replace(vo.content, newLine, "<br>") }</td>
+			</tr>
+		</table>
+		<br>
+	</c:forEach>
+</body>
+</html>
